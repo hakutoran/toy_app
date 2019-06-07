5184419d9ebea96b4c7742a7365c9e2504cf3d30
EC2 Default User <ec2-user@ip-172-31-37-145.ap-northeast-1.compute.internal>
2019-06-07 07:02:37 +0000

    Finish section2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  
+  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
    #githubのURLをここに記入しておきます
   #https://github.com/hakutoran/toy_app.git
-  root "application#hello"
+  #heroku   https://gentle-chamber-95817.herokuapp.com/  https://git.heroku.com/gentle-chamber-95817.git
+  
+  resources :users
+  root 'users#index'
 end
