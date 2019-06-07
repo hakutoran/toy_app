e44911059ab90801ebfca56feda44811cc6400af
EC2 Default User <ec2-user@ip-172-31-37-145.ap-northeast-1.compute.internal>
2019-06-07 02:04:14 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   #githubのURLをここに記入しておきます
+  #https://github.com/hakutoran/toy_app.git
+  root "application#hello"
 end
